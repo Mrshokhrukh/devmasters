401e8c1e0050367b3aa4f7829a6f7a49e8726f06
shokhrukh <114140248+Mrshokhrukh@users.noreply.github.com>
2023-11-30 23:18:48 +0500

    set up

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import logo from "../../assets/DeVmasters__1_-removebg-preview (1).png";
+import "./header.scss";
+const Header = () => {
+  return <div className="header">Header</div>;
+};
+
+export default Header;
