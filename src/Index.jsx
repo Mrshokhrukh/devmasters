5d3d384172a42b4b8dc6cabe2438f7883ffd06d4
shokhrukh <114140248+Mrshokhrukh@users.noreply.github.com>
2023-12-04 20:21:16 +0500

    ''ll

--- src/Index.jsx
+++ src/Index.jsx
@@ -1,4 +1,5 @@
 import Adverting from "./components/advert/Adverting";
+import Collabs from "./components/collaborators/Collabs";
 import InfoGram from "./components/infoGram/InfoGram";
 import Home from "./pages/home/Home";
 
@@ -7,7 +8,10 @@ const Index = () => {
     <>
       <Home />
       <InfoGram />
-      <Adverting />
+      <div className="ads_lenting">
+        <Adverting />
+        <Collabs />
+      </div>
     </>
   );
 };
---
+++ src/components/collaborators/Collabs.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "./collabs.scss";
+const Collabs = () => {
+  return (
+    <div>
+      <h2>asdsa</h2>
+    </div>
+  );
+};
+
+export default Collabs;
