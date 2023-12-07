8c2c591db49c73e4657dd83be040da4be1051707
shokhrukh <114140248+Mrshokhrukh@users.noreply.github.com>
2023-12-07 23:11:41 +0500

    prices

---
+++ src/components/priceCard/PriceCard.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+import { FaRegCircleCheck } from "react-icons/fa6";
+import "./priceCard.scss";
+const PriceCard = ({ num, title, explain, price, includes }) => {
+  return (
+    <div className={`priceCard_${num}`}>
+      <div className="priceCard_inner">
+        <div className="pr_card_top">
+          <p className="pr_card_title">{title}</p>
+          <span className="pr_card_explain">{explain}</span>
+        </div>
+        <div className="pr_card_info">
+          <div className="pr_card_price">
+            <p>${price}</p>
+          </div>
+          <div className="pr_card_includes">
+            <p className="include_ttl">Ichiga oladi:</p>
+            <div className="inc_desc">
+              {includes.map((item, i) => {
+                return (
+                  <p key={i}>
+                    <span>
+                      <FaRegCircleCheck />
+                    </span>
+                    {item}
+                  </p>
+                );
+              })}
+            </div>
+          </div>
+        </div>
+        <div className="pr_card_bottom">
+          <a href="">
+            <div className="pr_priceCard_btn">Ma'lumot olish</div>
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default PriceCard;
--- src/pages/prices/Prices.jsx
+++ src/pages/prices/Prices.jsx
@@ -1,9 +1,30 @@
-import React from 'react'
+import React from "react";
+import "./prices.scss";
+import { service_prices } from "../../assets/data";
+import PriceCard from "../../components/priceCard/PriceCard";
 
 const Prices = () => {
   return (
-    <div>Prices</div>
-  )
-}
+    <div className="container">
+      <h1 className="prices_page_title">
+        Siz uchun hamyonbop narxdagi <span>Veb Saytlar</span>
+      </h1>
+      <div className="prices_page">
+        {service_prices.map((item, i) => {
+          return (
+            <PriceCard
+              key={i}
+              num={item.num}
+              title={item.title}
+              explain={item.explain}
+              price={item.price}
+              includes={item.includes}
+            />
+          );
+        })}
+      </div>
+    </div>
+  );
+};
 
-export default Prices
+export default Prices;
