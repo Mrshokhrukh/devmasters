a80854aac1e53ccaaa556e8080c502631b8934fe
Mrshokhrukh <shokhrukhabduvokhidov@gmail.com>
2024-09-07 23:35:48 +0500

    form

--- src/components/contactForm/ContactForm.jsx
+++ src/components/contactForm/ContactForm.jsx
@@ -1,23 +1,49 @@
-import React from "react";
+import React, { useState } from "react";
 import "./contactForm.scss";
 
 const ContactForm = () => {
+  const [data, setData] = useState({
+    Name: "",
+    Phone: "",
+    Service: "",
+  });
+  const handleChange = (e) => {
+    setData({ ...data, [e.target.name]: e.target.value });
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(data);
+  };
   return (
     <div className="container" id="footer">
       <div className="contactForm form_field_container">
         <h2>Bizga aloqaga chiqing</h2>
-        <form>
+        <form onSubmit={handleSubmit}>
           <div>
             <label htmlFor="name">Ism</label>
-            <input type="text" id="name" placeholder="Ismingizni kiriting" />
+            <input
+              type="text"
+              id="name"
+              name="Name"
+              value={data.Name}
+              onChange={handleChange}
+              placeholder="Ismingizni kiriting"
+            />
           </div>
           <div>
             <label htmlFor="name">Telefon raqam</label>
-            <input type="number" id="phone" placeholder="Telefon raqamingizni kiriting" />
+            <input
+              type="number"
+              value={data.Phone}
+              onChange={handleChange}
+              id="phone"
+              name="Phone"
+              placeholder="Telefon raqamingizni kiriting"
+            />
           </div>
           <div>
             <label htmlFor="service">Hizmat turini tanlang</label>
-            <select name="service" id="service">
+            <select name="Service" value={data.Service} id="service" onChange={handleChange}>
               <option value="eCommerce">E-commerce</option>
               <option value="landingPage">Landing Page</option>
               <option value="crm">Crm Systems</option>
